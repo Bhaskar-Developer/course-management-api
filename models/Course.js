bfe620ab233b51ab5f0199f32a7c549e363915d4
Bhaskar-Developer <koderulzstack@gmail.com>
2022-09-07 22:28:17 +0530

    Update Course model.

--- models/Course.js
+++ models/Course.js
@@ -1,38 +1,41 @@
 const mongoose = require('mongoose')
+const { getVideoDurationInSeconds } = require('get-video-duration');
+
+const TopicsSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please add topic name']
+  },
+  duration: {
+    type: Number
+  },
+  videoUrl: {
+    type: String,
+    required: [true, 'Please specify video Url for topic']
+  }
+})
 
 const CourseSchema = new mongoose.Schema({
   title: {
     type: String,
     trim: true,
-    required: [true, 'Please add a course Title']
+    required: [true, 'Please add title']
   },
   description: {
     type: String,
     required: [true, 'Please add description']
   },
   totalDuration: {
-    type: Number
+    type: Number,
   },
   category: {
     type: String,
-    required: [true, 'Please specify category'],
+    required: [true, 'Please add category'],
   },
-  topics: [
-    {
-      name: {
-        type: String,
-        required: true
-      },
-      duration: {
-        type: Number,
-        required: true
-      },
-      videoUrl: {
-        type: String,
-        required: [true, 'Please specify video Url for topic']
-      }
-    }
-  ],
+  topics: {
+    type: [TopicsSchema],
+    validate: v => Array.isArray(v) && v.length > 0
+  },
   courseFiles: [
     {
       fileId: {
@@ -55,6 +58,20 @@ const CourseSchema = new mongoose.Schema({
     ref: 'User',
     required: true
   }
+})
+
+CourseSchema.pre('save', async function (next) {
+  const course = this //this is the current course that is about to be saved
+
+  // calculate duration of each topic in course and total duration of this course
+  course.totalDuration = 0.0;
+  for (let i = 0; i <= course.topics.length - 1; i++) {
+    course.topics[i].duration = await getVideoDurationInSeconds(course.topics[i].videoUrl)
+    console.log("Topic Duration: ", course.topics[i].duration)
+    course.totalDuration += course.topics[i].duration
+  }
+
+  next()
 })
 
 
