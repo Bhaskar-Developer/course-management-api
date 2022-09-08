cdb12fe6210b69f567cfa9224a3aebcb41ff71a1
Bhaskar-Developer <koderulzstack@gmail.com>
2022-09-08 12:29:40 +0530

    Add course deletion route and controller.

--- controllers/course.controller.js
+++ controllers/course.controller.js
@@ -1,6 +1,7 @@
 
 const Course = require('../models/Course')
 const asyncHandler = require('../middlewares/async')
+const errorResponse = require('../utils/errorResponse')
 
 
 //@desc     Add New Course
@@ -25,5 +26,32 @@ exports.createCourse = asyncHandler(async (req, res, next) => {
   res.status(200).json({
     success: true,
     message: "Course was sucessfully created."
+  })
+})
+
+
+//@desc     Delete a Course
+//@route    DELETE /api/v1/courses/:id
+//@access   Private
+exports.deleteCourse = asyncHandler(async (req, res, next) => {
+  //check if the course to be deleted exists in the database
+  const course = await Course.findById(req.params.id)
+
+  //if the course does not exist then return a 404 error
+  if (!course) {
+    return next(new errorResponse(`Course not found with the id of ${req.params.id}`, 404))
+  }
+
+  // Admins can delete only the courses that were created by them.
+  if (course.user.toString() !== req.user.id) {
+    return next(new errorResponse(`User with id ${req.user.id} is not authorized to delete course with id ${course._id}`, 401))
+  }
+
+  // Delete the course
+  await course.remove()
+
+  res.status(200).json({
+    success: true,
+    message: 'Course was successfully deleted.'
   })
 })
--- routes/course.route.js
+++ routes/course.route.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
-const { createCourse } = require('../controllers/course.controller')
+const { createCourse, deleteCourse } = require('../controllers/course.controller')
 const { protectRoute, authorize } = require('../middlewares/auth')
 
 router.post('/', protectRoute, authorize('admin'), createCourse)
+router.delete('/:id', protectRoute, authorize('admin'), deleteCourse)
 
 module.exports = router
