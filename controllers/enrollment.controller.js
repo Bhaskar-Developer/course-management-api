3c772993c11792e6d2a43ed21da64493972e55c2
Bhaskar-Developer <koderulzstack@gmail.com>
2022-09-09 15:26:22 +0530

    Add enrollment update route and controller.

--- controllers/enrollment.controller.js
+++ controllers/enrollment.controller.js
@@ -82,3 +82,56 @@ exports.getEnrollment = asyncHandler(async (req, res, next) => {
     message: "Enrollment was successfully fetched."
   })
 })
+
+//@desc     UPDATE Enrollment
+//@route    PATCH /api/v1/enrollments
+//@access   Private
+exports.updateEnrollment = asyncHandler(async (req, res, next) => {
+  // check if enrollment id  and topic are specified in request body
+  if (!req.body.enrollmentId || !req.body.topic) {
+    return next(new errorResponse(`Please specify enrollment id and topic`, 400))
+  }
+
+  // Check if enrollment exists
+  const singleEnrollment = await Enrollment.findOne({ _id: req.body.enrollmentId, user: req.user.id })
+  if (!singleEnrollment) {
+    return next(new errorResponse(`Enrollment not found with id ${req.body.enrollmentId}`, 404))
+  }
+
+  const topicToUpdate = singleEnrollment.topics.find((element) => element._id.toString() === req.body.topic.id)
+  const foundTopicIndex = singleEnrollment.topics.findIndex((element) => element._id.toString() === req.body.topic.id)
+
+  // check if topic exists
+  if (!topicToUpdate) {
+    return next(new errorResponse(`Topic not found with id ${req.body.topic.id}`, 404))
+  }
+
+  // Don't allow topic to be updated if it is already complete
+  if (topicToUpdate.topicCompleted) {
+    return next(new errorResponse(`Topic with id ${req.body.topic.id} is already completed by user with id ${req.user.id}`, 400))
+  }
+
+  // make sure progress is a number
+  if (isNaN(req.body.topic.progress)) {
+    return next(new errorResponse(`Invalid progress specified`, 400))
+  }
+
+  // update topic
+  topicToUpdate.progress += req.body.topic.progress
+  if (topicToUpdate.progress >= topicToUpdate.duration) {
+    // if topic progress is greater or equal to the duration of topic then it means the topic is completed
+    topicToUpdate.topicCompleted = true
+    topicToUpdate.progress = topicToUpdate.duration
+  }
+
+  // Update enrollment with updated topic
+  singleEnrollment.topics[foundTopicIndex] = topicToUpdate
+
+  // update changes to DB
+  await Enrollment.findOneAndUpdate({ _id: req.body.enrollmentId }, singleEnrollment)
+
+  res.status(200).json({
+    success: true,
+    message: "Enrollment was successfully updated."
+  })
+})
--- routes/enrollment.route.js
+++ routes/enrollment.route.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const router = express.Router()
 
-const { createEnrollment, getEnrollments, getEnrollment } = require('../controllers/enrollment.controller')
+const { createEnrollment, getEnrollments, getEnrollment, updateEnrollment } = require('../controllers/enrollment.controller')
 const { protectRoute, authorize } = require('../middlewares/auth')
 
 router.post('/', protectRoute, authorize('employee'), createEnrollment)
 router.get('/', protectRoute, authorize('employee'), getEnrollments)
 router.get('/:enrollmentId', protectRoute, authorize('employee'), getEnrollment)
+router.patch('/', protectRoute, authorize('employee'), updateEnrollment)
 
 
 module.exports = router
